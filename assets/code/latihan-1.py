36c858eb0d79d64637e0b8a46bd173e04cb96235
fajaragngn <fajaragungnugroho29@yahoo.com>
2020-11-08 21:28:53 +0700

    rebuilding task Sun Nov  8 21:28:53 WIB 2020

--- assets/code/latihan-1.py
+++ assets/code/latihan-1.py
@@ -1,9 +1,11 @@
 #bilangan bertingkat(nested)
-baris = 10
-kolom = baris
-
-for bar in range(baris):
-    for col in range(kolom):
-        tab = bar+col
-        print("{0:>5}".format(tab), end='')
+for row in range(0, 10):
+    for col in range(0, 10):
+        num = row + col
+        if num < 10:
+            empty = "  "
+        else:
+            if num < 100:
+                empty  = " "
+        print(empty, num, end = '')
     print()
